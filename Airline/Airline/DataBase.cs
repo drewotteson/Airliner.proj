69cc1666dd746f052c2560a3bc60d4763efe0a11
Nick Wilson <Nick Wilson>
2016-02-05 15:14:47 -0600

    created database class

--- Airline/Airline/DataBase.cs
+++ Airline/Airline/DataBase.cs
@@ -8,7 +8,7 @@ namespace Airline
 {
     public class DataBase
     {
-        Passenger passenger = new Passenger("Breezy", 32, 1000);
+        Passenger passenger = new Passenger("Breezy", "32", 1000);
         Seat seat = new Seat();
         Manifest manifest = new Manifest();
         Aircraft aircraft = new Aircraft(100, 1000, true);
